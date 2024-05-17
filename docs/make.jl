3b97abf627952d997bf78082950de7a7e1ec3d1f
Nathan Musoke <nathan.musoke@gmail.com>
2024-05-17 18:43:06 -0400

    chore: JuliaFormatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using PrideColorSchemes
 using Documenter
 
-DocMeta.setdocmeta!(PrideColorSchemes, :DocTestSetup, :(using PrideColorSchemes); recursive=true)
+DocMeta.setdocmeta!(
+    PrideColorSchemes,
+    :DocTestSetup,
+    :(using PrideColorSchemes);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[PrideColorSchemes],
-    authors="Nathan Musoke <nathan.musoke@gmail.com> and contributors",
-    sitename="PrideColorSchemes.jl",
-    format=Documenter.HTML(;
-        canonical="https://musoke.github.io/PrideColorSchemes.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [PrideColorSchemes],
+    authors = "Nathan Musoke <nathan.musoke@gmail.com> and contributors",
+    sitename = "PrideColorSchemes.jl",
+    format = Documenter.HTML(;
+        canonical = "https://musoke.github.io/PrideColorSchemes.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/musoke/PrideColorSchemes.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/musoke/PrideColorSchemes.jl", devbranch = "main")
