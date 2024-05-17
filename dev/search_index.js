ec3b4f345111878672eaa4ee8bb52fb4f5a47c1b
Documenter.jl <documenter@juliadocs.github.io>
2024-05-17 17:28:10 +0000

    build based on 350e8a8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PrideColorSchemes","category":"page"},{"location":"#PrideColorSchemes","page":"Home","title":"PrideColorSchemes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PrideColorSchemes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PrideColorSchemes]","category":"page"}]
+}
